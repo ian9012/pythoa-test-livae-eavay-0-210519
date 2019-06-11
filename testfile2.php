2aa8d405c9f9f2ea91dca18406e27fd8249e7150
ebarnad <eavay.barnad@yopeso.com>
2019-06-11 18:19:28 +0800

    Precondition1

---
+++ testfile2.php
@@ -0,0 +1,15 @@
+<?php
+class mock {
+
+        public function method1Updated()
+        {
+                //code goes heredasdsad updated
+        }
+
+        public function method2sdasda()
+        {
+                $variable = "string tests";
+                return $variable
+        }
+}
+
